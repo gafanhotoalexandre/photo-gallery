6174ab4d3a85cb91929a1b6148a87f81a91ae974
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-06-26 12:42:29 -0300

    apresentando feedback visual para grid de photos

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -18,3 +18,18 @@ export const Header = styled.h1`
   text-align: center;
   margin-bottom: 30px;
 `;
+
+export const ScreenWarning = styled.section`
+  text-align: center;
+
+  .emoji {
+    font-size: 50px;
+    margin-bottom: 20px;
+  }
+`;
+
+export const PhotoList = styled.section`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 10px;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
+import { useState, useEffect } from 'react';
+
+// services
+import * as Photos from './services/photos';
+
 import * as C from './App.styles';
+import { Photo } from './types/Photo';
 
 export default function App() {
+  const [loadingPhotos, setLoadingPhotos] = useState(false);
+  const [photos, setPhotos] = useState<Photo[]>([]);
+
+  useEffect(() => {
+    async function getPhotos() {
+      setLoadingPhotos(true);
+      // pegando imagens
+      setPhotos(await Photos.getAll());
+
+      setLoadingPhotos(false);
+    }
+    getPhotos();
+  }, []);
+
   return (
     <C.Container>
       <C.Area>
@@ -10,7 +30,27 @@ export default function App() {
         {/* Área de Upload */}
 
         {/* Lista de Fotos */}
+        {loadingPhotos &&
+          <C.ScreenWarning>
+            <div className="emoji">🤚</div>
+            <div className="">Carregando...</div>
+          </C.ScreenWarning>
+        }
 
+        {!loadingPhotos && photos.length > 0 &&
+          <C.PhotoList>
+            {photos.map((item, index) => (
+              <div>{item.name}</div>
+            ))}
+          </C.PhotoList>
+        }
+
+        {!loadingPhotos && photos.length === 0 &&
+          <C.ScreenWarning>
+            <div className="emoji">😞</div>
+            <div className="">Não há fotos cadastradas.</div>
+          </C.ScreenWarning>
+        }
       </C.Area>
     </C.Container>
   );
