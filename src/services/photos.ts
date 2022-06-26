97f8da6fb593de40db365150e29d2828dcd970d5
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-06-26 12:24:43 -0300

    finalizando service de photos

--- src/services/photos.ts
+++ src/services/photos.ts
@@ -9,7 +9,14 @@ export async function getAll(): Promise<Photo[]> {
   const imagesFolder = ref(storage, 'images');
   const photoList = await listAll(imagesFolder);
 
+  for (let i in photoList.items) {
+    let photoUrl = await getDownloadURL(photoList.items[i]);
 
+    list.push({
+      name: photoList.items[i].name,
+      url: photoUrl,
+    });
+  }
 
   return list;
 }
